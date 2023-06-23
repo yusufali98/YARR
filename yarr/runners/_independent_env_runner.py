72c37e6d31ff1b1d7ce131be5c8f80f66ee271d8
Yu-Wei Chao <ychao@nvidia.com>
2023-06-23 14:17:16 -0700

    Add copyright header

--- yarr/runners/_independent_env_runner.py
+++ yarr/runners/_independent_env_runner.py
@@ -1,3 +1,16 @@
+# Not a contribution
+# Changes made by NVIDIA CORPORATION & AFFILIATES enabling RVT or otherwise documented as
+# NVIDIA-proprietary are not a contribution and subject to the following terms and conditions:
+#
+# Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+# property and proprietary rights in and to this material, related
+# documentation and any modifications thereto. Any use, reproduction,
+# disclosure or distribution of this material and related documentation
+# without an express license agreement from NVIDIA CORPORATION or
+# its affiliates is strictly prohibited.
+
 import copy
 import logging
 import os
